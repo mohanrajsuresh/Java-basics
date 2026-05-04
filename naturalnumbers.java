f3bbc64c4ae528b32b7b69d7c428292f9a7bc9cb
mohanrajsuresh <mohanrajsuresh2007@gmail.com>
2026-05-04 22:15:58 +0530

    Add files via upload

---
+++ naturalnumbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class naturalnumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Sc.nextInt();
+        for(int i = 1; i<n ; i++){
+            System.out.println(i + " ");
+        }
+    }
+
+}
